25bdb15f6f10661242cd6cdfc90fbb2ed1f7cd8a
tornworm <42328527+tornworm@users.noreply.github.com>
2019-03-11 14:04:34 +0800

    set UI_Main

---
+++ Assets/UI_Main.cs
@@ -0,0 +1,20 @@
+﻿//this is a C#
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class UI_Main : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+}
---
+++ Assets/testsss.cs
@@ -0,0 +1,18 @@
+﻿//this is a C#
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class testsss : MonoBehaviour {
+
+	
+	
+	
+	
+}
