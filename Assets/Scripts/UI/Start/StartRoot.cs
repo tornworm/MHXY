ab7d99d41b08635c1a2ceb543e33527422d99419
tornworm <42328527+tornworm@users.noreply.github.com>
2019-03-11 22:39:25 +0800

    create open CG
    s

---
+++ Assets/Scripts/UI/Start/StartRoot.cs
@@ -0,0 +1,37 @@
+﻿//this is a C#
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StartRoot : MonoBehaviour {
+
+
+
+
+    private GameObject startPanel;//开始界面
+    private GameObject Video;//过场动画物体
+    private VideoPlayer videoPlayer;//过场动画物体Video组件
+	void Start () {
+        startPanel = transform.Find("UI_StartMain").gameObject;
+        Video = transform.Find("opening").gameObject;
+
+        videoPlayer= Video.GetComponent<VideoPlayer>();
+	}
+    private void Update()
+    {
+
+        if (Input.anyKeyDown)
+        {
+            videoPlayer.Stop();
+            startPanel.SetActive(true);
+            Video.SetActive(false);
+        }
+    }
+   
+
+
+}
