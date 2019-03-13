44d05f8b40027cf00224c16a71a34e4dacf3cdb4
MoYueLi <48431321+MoYueLi@users.noreply.github.com>
2019-03-13 21:26:08 +0800

    Finish UI_Welfare
    wanchengUI_Welfare

---
+++ Assets/Scripts/UI/Welfare/UI_WelfarePanel5Cell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_WelfarePanel5Cell : MonoBehaviour {
+
+    private int id;
+    private Text Level;
+    private Text Level2;
+
+    void Start()
+    {
+        Find();
+        id = int.Parse(name);
+        Show();
+    }
+
+    void Find()
+    {
+        Level = transform.Find("Text1").GetComponent<Text>();
+        Level2 = transform.Find("Image/Text1").GetComponent<Text>();
+    }
+
+    void Show()
+    {
+        Level.text = id.ToString();
+        Level2.text = id.ToString();
+    }
+
+
+
+}
