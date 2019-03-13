3ef0f83b95d48df011af83047e148e9b737cb2a2
MoYueLi <48431321+MoYueLi@users.noreply.github.com>
2019-03-13 15:33:39 +0800

    three Panel
    qian3gemokuaiwancheng

---
+++ Assets/Scripts/UI/Welfare/WelfareManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelfareManager : MonoBehaviour {
+
+	private static WelfareManager singeton;
+    public static WelfareManager Singeton
+    {
+        get
+        {
+            if (singeton == null)
+            {
+                singeton = new WelfareManager();
+            }
+            return singeton;
+        }
+    }
+    private WelfareManager() { }
+
+    public int Panel1Count = 7;
+
+}
